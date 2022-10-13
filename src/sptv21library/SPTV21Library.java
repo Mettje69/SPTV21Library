740ab82fda218e06856529bd146628268aeb2cb7
teacher <you@example.com>
2022-10-13 11:27:28 +0300

    Создали прототип библиотеки (математическую модель)

--- src/sptv21library/SPTV21Library.java
+++ src/sptv21library/SPTV21Library.java
@@ -15,7 +15,8 @@ public class SPTV21Library {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        App app = new App();
+        app.run();
     }
     
 }
